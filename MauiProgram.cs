c54f78696066c3c8ccd6db10f7955a043eed2c8b
Fırat Kazak <firatkazak@gmail.com>
2024-07-16 12:15:18 +0300

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.AddFont("Sublima-ExtraBold.otf", "SublimaExtraBold");
+            fonts.AddFont("Sublima-Light.otf", "SublimaLight");
+        });
+        return builder.Build();
+    }
+}
